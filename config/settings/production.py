ab6e2db51280f92a9c806f8f2a7af237227f5609
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2022-09-04 13:56:31 +0300

    Congiuration Django Project

---
+++ config/settings/production.py
@@ -0,0 +1,19 @@
+from config.settings.base import *
+
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
